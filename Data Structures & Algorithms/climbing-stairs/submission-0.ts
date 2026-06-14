53319019708ce51e7486a4218bc110f01dcc4a4f
Ryan Nekadio <145487234+RyanNekadio@users.noreply.github.com>
2026-06-14 14:23:19 +0100

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n: number): number {
+        if (n <= 2) return n;
+
+        const dp: number[] = new Array(n + 1).fill(0);
+
+        // No. of ways to reach step 1
+        dp[1] = 1;
+        // No. of ways to reach step 2
+        dp[2] = 2;
+
+        for (let i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+
+
+    }
+}
