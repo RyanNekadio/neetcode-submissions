fafca0c308d971b3ab9c49828238ed878a2e5f31
Ryan Nekadio <145487234+RyanNekadio@users.noreply.github.com>
2026-06-14 08:36:02 +0100

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.ts
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @param {number} n
+     * @return {number}
+     */
+    myPow(x: number, n: number): number {
+        return x**n;
+    }
+}
